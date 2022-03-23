32c66d363a669cd3e8c3ed9cfe0219da1642c3d5
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-03-23 13:11:43 +0600

    update

--- update.php
+++ update.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 $id = $_GET['id'];
 
 $name = $_POST['name'];
 $roll = $_POST['roll'];
-$dob = $_POST['dob'];
+$dob  = $_POST['dob'];
 
 require 'connect.php';
 
@@ -12,8 +12,8 @@ $sql = "UPDATE students
 		SET name = '$name', roll = $roll, dob = '$dob'
 		WHERE id = $id ";
 
-if ( $conn->query($sql) ) {
-	session_start();
-	$_SESSION['msg'] = 'Update Successfully.';
-	header("Location: show.php?id=" . $id);
+if ( $conn->query( $sql ) ) {
+    session_start();
+    $_SESSION['msg'] = 'Update Successfully.';
+    header( "Location: show.php?id=" . $id );
 }
